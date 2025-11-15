f6a878294acf65bff851cc5a51438c4ea3130a2f
beaver <alex101ki@gmail.com>
2025-11-15 11:44:37 +0200

    git init

---
+++ src/main/java/com/alexki/simplejavabot/SimpleJavaBotApplication.java
@@ -0,0 +1,13 @@
+package com.alexki.simplejavabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleJavaBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleJavaBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alexki/simplejavabot/SimpleJavaBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexki.simplejavabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleJavaBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
